6be7e3425c8d40dc647cf03b2f106167cad428e8
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-08-02 16:03:30 -0600

    Add responsiveness to navegation bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,71 +1,71 @@
 import cryptoMan from "../assets/cryptoman.jpg";
 import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
-import "../styles/NavBar.css"
+import "../styles/NavBar.css";
+import { MenuOutlined } from "@ant-design/icons";
 
 export default function NavBar() {
   const location = useLocation();
   const [activeLink, setActiveLink] = useState(location.pathname);
-  const [activeMenu, setActiveMenu] = useState(true);
-  const [screenSize, setScreenSize] = useState(null);
+  const [showNav, setShowNav] = useState(false);
 
   const handleLinkClick = (path: string) => {
     setActiveLink(path);
+    handleShowNavbar();
   };
+
+  const handleShowNavbar = () => setShowNav(!showNav);
 
   useEffect(() => {
     setActiveLink(location.pathname);
   }, [location]);
 
-  const handleResize = () => {
-    setScreenSize(window.innerWidth);
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-
-    handleResize();
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, [])
-
-  useEffect(() => {
-    if(screenSize < 768){
-      setActiveMenu(false);
-    }
-  }, [screenSize])
-
   return (
     <nav className="navbar">
-      <div className="logo-container">
-        <div className="logo-img-container">
-          <img src={cryptoMan} alt="" className="logo-img" />
+      <div className="container">
+        <Link to="/">
+          <div className="logo-container">
+            <div className="logo-img-container">
+              <img src={cryptoMan} alt="" className="logo-img" />
+            </div>
+            <h1 className="logo-text">CryptoMan</h1>
+          </div>
+        </Link>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <MenuOutlined />
         </div>
-        <h1 className="logo-text">CryptoMan</h1>
-      </div>
-      <div className="nav-right">
-        <div className="nav-links">
-          <Link
-            to="/"
-            className={activeLink === "/" ? "active" : ""}
-            onClick={() => handleLinkClick("/")}
-          >
-            Home
-          </Link>
-          <Link
-            to="/cryptos"
-            className={activeLink.startsWith("/cryptos") ? "active" : ""}
-            onClick={() => handleLinkClick("/cryptos")}
-          >
-            Cryptos
-          </Link>
-          <Link
-            to="/news"
-            className={activeLink.startsWith("/news") ? "active" : ""}
-            onClick={() => handleLinkClick("/news")}
-          >
-            News
-          </Link>
+        <div className={`nav-links ${showNav && "active"}`}>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className={activeLink === "/" ? "active-link" : ""}
+                onClick={() => handleLinkClick("/")}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/cryptos"
+                className={
+                  activeLink.startsWith("/cryptos") ? "active-link" : ""
+                }
+                onClick={() => handleLinkClick("/cryptos")}
+              >
+                Cryptos
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/news"
+                className={activeLink.startsWith("/news") ? "active-link" : ""}
+                onClick={() => handleLinkClick("/news")}
+              >
+                News
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
